a0e4e72202fc23cc7dd953078ded8ae44334fc38
mahdikhashan <mahdikhashan1@gmail.com>
2025-02-19 13:11:22 +0100

    mock is working, logic is wrong

--- go.mod
+++ go.mod
@@ -2,11 +2,4 @@ module example.com/sumfile
 
 go 1.23.5
 
-require github.com/stretchr/testify v1.10.0
-
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.5.2 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
-)
+require go.uber.org/mock v0.5.0
--- sumfile_test.go
+++ sumfile_test.go
@@ -1,23 +1,19 @@
 package main
 
-import (
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
+import(
 	"testing"
-	"example.com/sumfile/mocks"
+
+	"go.uber.org/mock/gomock"
 )
 
-// TODO: use mockery and pass mocked io.Read interface
 func Test_(t *testing.T) {
 	str := "0\n2\n3\n4\n5\n"
-	r := new(mocks.IoReader)
-	r.
-		On("Read", mock.Anything).
-		Run(func(args mock.Arguments) {
-			bytes := args[0].([]byte)
-			copy(bytes[:], str)
-		}).
-		Return(len(str), nil)
+	ctrl := gomock.NewController(t)
 
-	assert.Equal(t, "0\n1\n3\n4\n5\n", sumfile("tmp.txt"), `should be 1 2 3 4 5`)
+	m := NewMockioReader(ctrl)
+
+	m.
+		EXPECT().
+		Read("temp.txt").
+		Return(str)
 }
